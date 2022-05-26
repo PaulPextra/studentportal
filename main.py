006f97e06744af400d2f9d7c63714b29ad177c9b
Paul Okoli <paulokoli7@gmail.com>
2022-05-26 18:21:43 +0100

    Student Portal

--- main.py
+++ main.py
@@ -107,8 +107,3 @@ def change_status(id):
     cur.close()
     
     return json.dumps('success')
-
-
-# Running the app
-if __name__ == '__main__':
-    app.run() 
---
+++ wsgi.py
@@ -0,0 +1,5 @@
+from main import app
+
+# Running the app
+if __name__ == '__main__':
+    app.run() 
